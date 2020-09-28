e7eb729423daac906a2c7cbaaaf9af90464dc7e1
Hussein Allaw <allawhussein@gmail.com>
2020-09-28 18:18:00 +0300

    created Pair to store x and y values

---
+++ Hussein/Odin_Back-end/Data_Structure_Project/knight_move.rb
@@ -0,0 +1,11 @@
+class Pair
+    attr_accessor :x_value
+    attr_accessor :y_value
+
+    def initialize x_value = nil, y_value = nil
+        @x_value = x_value
+        @y_value = y_value
+    end
+end
+
+def move_array ()
